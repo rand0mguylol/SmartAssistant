30e3f6f4e8db7ecca8c876a299b6d5be99bac40d
Nixon <voxell.technologies@gmail.com>
2020-12-11 18:55:19 +0800

    added features to Audio Visualizer

--- Assets/Scripts/AudioVisualizer.cs
+++ Assets/Scripts/AudioVisualizer.cs
@@ -2,35 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioVisualizer : MonoBehaviour
 {
-    public AudioSource source;
-    public Material material;
-    // Start is called before the first frame update
-    void Start()
+  public AudioSource audioSource;
+  public AudioClip audioClip;
+  public static float[] audioSpectrum;
+
+  public FFTWindow fft;
+  
+  public uint sampleNum = 512;
+  public float amplitudeMultiplier = 10f;
+
+  public Material material;
+  // Start is called before the first frame update
+  void Start()
+  {
+    // audioSource = GetComponent<AudioSource>();
+    audioClip = audioSource.clip;
+    print(audioClip.frequency);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // float[] audioSpectrum = new float[1024];
+    // float[] audioOutput = new float[1024];
+
+    audioSpectrum = new float[sampleNum];
+    audioSource.GetSpectrumData(audioSpectrum, 0, fft);
+
+
+    for (int i = 1; i < audioSpectrum.Length - 1; i++)
     {
-        
+      Debug.DrawLine(new Vector3((i - 1)*0.01f + 1, audioSpectrum[i] * amplitudeMultiplier + 10), new Vector3(i *0.01f + 1, audioSpectrum[i + 1] * amplitudeMultiplier + 10), Color.cyan);
+      // Debug.DrawLine(new Vector3(i - 1, audioSpectrum[i] * 20 + 10, 0), new Vector3(i, audioSpectrum[i + 1] * 20 + 10, 0), Color.red);
+      // Debug.DrawLine(new Vector3(i - 1, Mathf.Log(audioSpectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(audioSpectrum[i]) + 10, 2), Color.cyan);
+      // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), audioSpectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), audioSpectrum[i] - 10, 1), Color.green);
+      // Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(audioSpectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(audioSpectrum[i]), 3), Color.blue);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float value = 0;
-        float[] spectrum = new float[256];
-
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-
-
-
-        for (int i = 1; i < spectrum.Length - 1; i++)
-        {
-            value += spectrum[i];
-            Debug.DrawLine(new Vector3(i - 1, spectrum[i] * 20 + 10, 0), new Vector3(i, spectrum[i + 1] * 20 + 10, 0), Color.red);
-            Debug.DrawLine(new Vector3(i - 1, Mathf.Log(spectrum[i - 1]) + 10, 2), new Vector3(i, Mathf.Log(spectrum[i]) + 10, 2), Color.cyan);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), spectrum[i - 1] - 10, 1), new Vector3(Mathf.Log(i), spectrum[i] - 10, 1), Color.green);
-            Debug.DrawLine(new Vector3(Mathf.Log(i - 1), Mathf.Log(spectrum[i - 1]), 3), new Vector3(Mathf.Log(i), Mathf.Log(spectrum[i]), 3), Color.blue);
-        }
-        material.SetFloat("_Value", value);
-        Debug.Log(value);
-    }
+  }
 }
