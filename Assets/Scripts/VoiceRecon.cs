d3c0c77738bc6bfdfc52cc11e7be41bb1d3ddbc0
Vincexodus <vincent4552@gmail.com>
2020-12-17 20:52:54 +0800

    Added Keyword Recognizer

---
+++ Assets/Scripts/VoiceRecon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceRecon : MonoBehaviour
+{
+  private KeywordRecognizer keywordRecognizer;
+  [SerializeField]
+  private string[]keywords;
+  private Dictionary<string,Action> keywordDict; //format of dictionary
+  
+  void Start()
+  {
+    keywordDict = new Dictionary<string,Action>(); // new dictionary
+    keywordDict.Add("hello", greet);
+
+    keywordRecognizer = new KeywordRecognizer(keywordDict.Keys.ToArray());
+    keywordRecognizer.OnPhraseRecognized += OnKeyWordRecognize;
+    keywordRecognizer.Start();
+  }
+
+  private void OnKeyWordRecognize(PhraseRecognizedEventArgs speech)
+  {
+    Debug.Log(speech.text);
+  }
+
+  private void greet()
+  {
+    print("hello there");
+  }
+}
