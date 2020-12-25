1f5daa8307268b42639d26238d5ab62a76187e42
Nixon <voxell.technologies@gmail.com>
2020-12-25 22:52:07 +0800

    Audio Visualizer is here!!!

--- Assets/Modules/Audio/Scripts/Agent.cs
+++ Assets/Modules/Audio/Scripts/Agent.cs
@@ -9,14 +9,16 @@ public partial class Agent : MonoBehaviour
   private const float epsilon = 0.001f;
   
   #region VFX Property IDs
-  private int audio1,
-  audio2,
-  audio3,
-  audio4,
-  audio5,
-  audio6,
-  audio7,
-  audio8,
+  private int noisePositionAddition,
+  radius,
+  // audio1,
+  // audio2,
+  // audio3,
+  // audio4,
+  // audio5,
+  // audio6,
+  // audio7,
+  // audio8,
   noiseIntensity;
   #endregion
 
@@ -41,14 +43,16 @@ public partial class Agent : MonoBehaviour
 
   void InitVFXPropertyIDs()
   {
-    audio1 = Shader.PropertyToID("Audio1");
-    audio2 = Shader.PropertyToID("Audio2");
-    audio3 = Shader.PropertyToID("Audio3");
-    audio4 = Shader.PropertyToID("Audio4");
-    audio5 = Shader.PropertyToID("Audio5");
-    audio6 = Shader.PropertyToID("Audio6");
-    audio7 = Shader.PropertyToID("Audio7");
-    audio8 = Shader.PropertyToID("Audio8");
+    // audio1 = Shader.PropertyToID("Audio1");
+    // audio2 = Shader.PropertyToID("Audio2");
+    // audio3 = Shader.PropertyToID("Audio3");
+    // audio4 = Shader.PropertyToID("Audio4");
+    // audio5 = Shader.PropertyToID("Audio5");
+    // audio6 = Shader.PropertyToID("Audio6");
+    // audio7 = Shader.PropertyToID("Audio7");
+    // audio8 = Shader.PropertyToID("Audio8");
+    radius = Shader.PropertyToID("Radius");
+    noisePositionAddition = Shader.PropertyToID("NoisePositionAddition");
     noiseIntensity = Shader.PropertyToID("Intensity");
   }
 }
--- Assets/Modules/Audio/Scripts/AudioVisualizer.cs
+++ Assets/Modules/Audio/Scripts/AudioVisualizer.cs
@@ -7,39 +7,14 @@ public partial class Agent : MonoBehaviour
   [Header("Audio Settings")]
   public AudioSource audioSource;
   public AudioClip audioClip;
-  public FFTWindow fft;
+  public FFTWindow window;
 
   public const int audioHertz = 44100;
-  public const int sampleSize = 1024;
-  public static float[] spectrumLeft;
-  public static float[] spectrumRight;
+  public const int samples = 1024;
+  public const float audioProfile = 0.0f;
 
-  public const int bandSize = 8;
-  public static float[] bandLeft;
-  public static float[] bandRight;
-  #endregion
+  public const int frequencyBands = 8;
 
-  #region Mic Settings
-  public static string micDevice;
-  #endregion
-
-  #region Audio Visualizer
-  #endregion
-
-  #region Editor Stuffs
-  [HideInInspector]
-  public bool showAudioSettings,
-  showMicSettings,
-  showAudioVisualizer;
-  #endregion
-
-  [SerializeField] private float audioProfile = 0.0f;
-  [SerializeField] private int samples = 512;
-  [SerializeField] private int frequencyBands = 8;
-  [SerializeField] private FFTWindow window = FFTWindow.Blackman;
-
-  // cached
-  private AudioSource cached_AudioSource;
   private float[] cached_Samples;
   private float[] cached_FreqBands;
   private float[] cached_FreqBandBuffer;
@@ -51,15 +26,28 @@ public partial class Agent : MonoBehaviour
   private float cached_Amplitude;
   private float cached_AmplitudeBuffer;
   private float cached_AmplitudeHighest;
+  #endregion
 
-  public float[] Samples
-  {
-    get { return cached_Samples; }
-  }
+  #region Mic Settings
+  public static string micDevice;
+  #endregion
 
-  void Awake()
+  #region Audio Visualizer
+  [Header("Audio Visualizer")]
+  public float radiusIncrement = 0.05f;
+  private float defaultRadius;
+  private float noisePosition;
+  #endregion
+
+  #region Editor Stuffs
+  [HideInInspector]
+  public bool showAudioSettings,
+  showMicSettings,
+  showAudioVisualizer;
+  #endregion
+
+  void InitAudioVisualizer()
   {
-    cached_AudioSource = GetComponent<AudioSource>();
     cached_Samples = new float[samples];
     cached_FreqBands = new float[frequencyBands];
     cached_FreqBandBuffer = new float[frequencyBands];
@@ -67,51 +55,25 @@ public partial class Agent : MonoBehaviour
     cached_FreqBandHighest = new float[frequencyBands];
     cached_AudioBand = new float[frequencyBands];
     cached_AudioBandBuffer = new float[frequencyBands];
+    InitializeAudioProfile(audioProfile);
 
-    print(SystemInfo.maxComputeWorkGroupSizeX);
+    noisePosition = 0.0f;
+    defaultRadius = audioVFX.GetFloat(radius);
+
+    micDevice = Microphone.devices[0].ToString();
+    audioSource.Play();
   }
 
   void UpdateAudioVisualizer()
   {
-    audioSource.GetSpectrumData(spectrumLeft, 0, fft);
-    audioSource.GetSpectrumData(spectrumRight, 1, fft);
-
-    CreateFreqBand(ref bandLeft, ref spectrumLeft);
-    CreateFreqBand(ref bandRight, ref spectrumRight);
-
     GenerateFrequencyBands();
     GenerateAudioBands();
     GenerateAmplitude();
-  }
 
-  public float GetAmplitude()
-  {
-    return cached_Amplitude;
-  }
-
-  public float GetAmplitudeBuffer()
-  {
-    return cached_AmplitudeBuffer;
-  }
-
-  public float GetFrequencyBand(int i)
-  {
-    return cached_FreqBands[i];
-  }
-
-  public float GetFrequencyBandBuffer(int i)
-  {
-    return cached_FreqBandBuffer[i];
-  }
-
-  public float GetAudioBand(int i)
-  {
-    return cached_AudioBand[i];
-  }
-
-  public float GetAudioBandBuffer(int i)
-  {
-    return cached_AudioBandBuffer[i];
+    noisePosition += cached_Amplitude * 0.1f;
+    audioVFX.SetFloat(noisePositionAddition, noisePosition);
+    audioVFX.SetFloat(noiseIntensity, cached_Amplitude + cached_AudioBand[0]);
+    audioVFX.SetFloat(radius, defaultRadius + Mathf.Lerp(0.0f, radiusIncrement, cached_AudioBand[0]));
   }
 
   private void InitializeAudioProfile(float value)
@@ -166,69 +128,40 @@ public partial class Agent : MonoBehaviour
     // 4000 - 6000
     // 6000 - 20000
 
-    cached_AudioSource.GetSpectrumData(cached_Samples, 0, FFTWindow.Blackman);
+    audioSource.GetSpectrumData(cached_Samples, 0, window);
 
-    int count = 0;
-    for (int i = 0; i < frequencyBands; ++i)
-    {
-      int sampleCount = (int)Mathf.Pow(2, i) * 2;
-      if (i == 7) sampleCount += 2;
-
-      float average = 0.0f;
-      for (int j = 0; j < sampleCount; ++j)
-      {
-        average += cached_Samples[count] * (count + 1);
-        ++count;
-      }
-      average /= count;
-      cached_FreqBands[i] = average;
-    }
-
-    ModulateFrequencyBands();
+    CreateFreqBand(ref cached_FreqBands, ref cached_Samples);
+    ModulateFrequencyBands(ref cached_FreqBandBuffer, ref cached_FreqBufferDecrease);
   }
 
-  private void ModulateFrequencyBands()
+  private void ModulateFrequencyBands(ref float[] freqBandBuffer, ref float[] freqBufferDecrease)
   {
-    for (int i = 0; i < cached_FreqBandBuffer.Length; ++i)
+    for (int i = 0; i < freqBandBuffer.Length; ++i)
     {
-      if (cached_FreqBands[i] > cached_FreqBandBuffer[i])
+      if (cached_FreqBands[i] > freqBandBuffer[i])
       {
-        cached_FreqBandBuffer[i] = cached_FreqBands[i];
+        freqBandBuffer[i] = cached_FreqBands[i];
         cached_FreqBufferDecrease[i] = 0.005f;
       }
 
-      if (cached_FreqBands[i] < cached_FreqBandBuffer[i])
+      if (cached_FreqBands[i] < freqBandBuffer[i])
       {
-        cached_FreqBandBuffer[i] -= cached_FreqBufferDecrease[i];
-        cached_FreqBufferDecrease[i] *= 1.2f;
+        freqBandBuffer[i] -= freqBufferDecrease[i];
+        freqBufferDecrease[i] *= 1.2f;
       }
     }
-  }
-  
-  void InitAudioVisualizer()
-  {
-    spectrumLeft = new float[sampleSize];
-    spectrumRight = new float[sampleSize];
-    bandLeft = new float[bandSize];
-    bandRight = new float[bandSize];
-
-    micDevice = Microphone.devices[0].ToString();
-
-    audioSource.Play();
-
-    InitializeAudioProfile(audioProfile);
   }
 
   void CreateFreqBand(ref float[] band, ref float[] spectrum)
   {
     int count = 0;
 
-    for (int i=0; i < bandSize; i++)
+    for (int i=0; i < frequencyBands; i++)
     {
       int sampleCount = (int) Mathf.Pow(2, i) * 2;
       if (i == 7) sampleCount += 2;
 
-      float average = 0;
+      float average = 0.0f;
       for (int s=0; s < sampleCount; s++)
       {
         average += spectrum[count]*(count + 1);
@@ -242,7 +175,7 @@ public partial class Agent : MonoBehaviour
 
   Vector4 SetForce(Vector3 position, ref float force)
   {
-    return new Vector4(position.x, position.y, position.z, Mathf.Clamp(force, 0, 1.0f));
+    return new Vector4(position.x, position.y, position.z, Mathf.Clamp(force, 0, 10.0f));
   }
 
   void OnDrawGizmos()
