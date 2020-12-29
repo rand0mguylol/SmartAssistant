f1d6dedc72eaa67cbe870735b285479a5647fd19
Nixon <voxell.technologies@gmail.com>
2020-12-29 15:39:56 +0800

    started speech

---
+++ PythonAPI/core/tests/socket_test.py
@@ -0,0 +1,14 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("localhost", 8052))
+
+client.send(b"fuck you server")
+
+while True:
+  try:
+    server_msg = client.recv(1024)
+    print(server_msg);
+  except KeyboardInterrupt:
+    client.close()
+    print("Disconnected")
