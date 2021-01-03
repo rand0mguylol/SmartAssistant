1f2c7c06432717e5403dc99102630b528c22430d
Nixon <voxell.technologies@gmail.com>
2021-01-03 18:43:35 +0800

    super dynamic audio visualizer is in place

--- Assets/Modules/Audio/Scripts/AudioProfile.cs
+++ Assets/Modules/Audio/Scripts/AudioProfile.cs
@@ -13,7 +13,9 @@ namespace Audio
     public float scale;
     public float sensitivity;
     public int smoothingIterations;
-    [Range(100, 20000)]
-    public float freqRange;
+    [Range(5000, 20000)]
+    public int freqRange;
+    [Range(10, 100)]
+    public int bandSize;
   }
 }
--- Assets/Modules/Audio/Scripts/AudioVisualizerDebug.cs
+++ Assets/Modules/Audio/Scripts/AudioVisualizerDebug.cs
@@ -15,15 +15,30 @@ public class AudioVisualizerDebug : MonoBehaviour
   public float width;
   public float xOffset;
 
+  int freqRange;
+  int bandSize;
+
   void Awake()
   {
     audioVisualizer = new AudioVisualizer(ref audioSource, ref audioProfile);
     spectrumSmoother = new SpectrumSmoother(ref audioVisualizer.freqSize, ref smoothingIterations);
+    bandSize = audioProfile.bandSize;
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (audioProfile.freqRange != freqRange)
+    {
+      audioVisualizer.Init();
+      freqRange = audioProfile.freqRange;
+    }
+    if (audioProfile.bandSize != bandSize)
+    {
+      audioVisualizer.Init();
+      bandSize = audioProfile.bandSize;
+    }
+
     audioVisualizer.SampleSpectrum();
     audioVisualizer.RescaleSamples(Time.deltaTime);
   }
@@ -34,7 +49,7 @@ public class AudioVisualizerDebug : MonoBehaviour
     {
       Gizmos.color = Color.cyan;
       Gizmos.color *= new Color(1, 1, 1, 0.5f);
-      for (int s=0; s < audioVisualizer.freq.Length; s++)
+      for (int s=0; s < audioVisualizer.band.Length; s++)
         Gizmos.DrawCube(transform.position + new Vector3(width*s + xOffset, 0, 0), new Vector3(width, audioVisualizer.freq[s], width));
     }
     
