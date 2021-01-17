b5aca9342a1215de634c71c6e339cd283333aec1
Nixon <voxell.technologies@gmail.com>
2021-01-18 02:08:54 +0800

    reduce duplicate requirements.txt to one

--- PythonAPI/setup/win/setup.py
+++ PythonAPI/setup/win/setup.py
@@ -24,10 +24,12 @@ def pipinstall(package):
 def pipwininstall(package):
   subprocess.check_call([sys.executable, "-m", "pipwin", "install", package])
 
-with open("./requirements.txt", "r") as f:
+with open("../requirements.txt", "r") as f:
   packages = f.read().split("\n")
   for p in packages:
     pipinstall(p)
+
+pipinstall("pipwin")
 
 with open("./pipwinrequirements.txt", "r") as f:
   packages = f.read().split("\n")
