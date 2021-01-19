478dc6ab7cfbee1728bc27f930407ec9f41b1a99
Nixon <voxell.technologies@gmail.com>
2021-01-19 12:53:21 +0800

    added TensorSharp & created examples for tts_engine

--- PythonAPI/core/tts_engine/examples/fastspeech2_example.py
+++ PythonAPI/core/tts_engine/examples/fastspeech2_example.py
@@ -21,37 +21,39 @@ from tensorflow_tts.inference import AutoConfig
 from tensorflow_tts.inference import TFAutoModel
 from tensorflow_tts.inference import AutoProcessor
 
-processor = AutoProcessor.from_pretrained("./ljspeech_mapper.json")
+processor = AutoProcessor.from_pretrained("../ljspeech_mapper.json")
 
-# initialize fastspeech model.
-fs_config = AutoConfig.from_pretrained('./fastspeech/conf/fastspeech.v1.yaml')
-fastspeech = TFAutoModel.from_pretrained(
-  config=fs_config,
-  pretrained_path="./fastspeech/checkpoints/model-195000.h5"
+# initialize fastspeech2 model.
+config = AutoConfig.from_pretrained('../fastspeech2/conf/fastspeech2.v1.yaml')
+fastspeech2 = TFAutoModel.from_pretrained(
+    config=config, 
+    pretrained_path="../fastspeech2/checkpoints/model-150000.h5",
+    is_build=True,
+    name="fastspeech2"
 )
 
 # initialize melgan model
-melgan_config = AutoConfig.from_pretrained('./melgan/conf/melgan.v1.yaml')
+melgan_config = AutoConfig.from_pretrained('../melgan/conf/melgan.v1.yaml')
 melgan = TFAutoModel.from_pretrained(
   config=melgan_config,
-  pretrained_path="./melgan/checkpoints/generator-1500000.h5"
+  pretrained_path="../melgan/checkpoints/generator-1500000.h5"
 )
 
+input_text = "how much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+input_ids = processor.text_to_sequence(input_text)
 
-ids = processor.text_to_sequence("how much wood would a woodchuck chuck if a woodchuck could chuck wood?")
-print(ids)
-ids = tf.expand_dims(ids, 0)
-# fastspeech inference
-
-masked_mel_before, masked_mel_after, duration_outputs = fastspeech.inference(
-  ids,
-  speaker_ids=tf.zeros(shape=[tf.shape(ids)[0]], dtype=tf.int32),
-  speed_ratios=tf.constant([1.0], dtype=tf.float32)
+# fastspeech2 inference
+mel_before, mel_after, duration_outputs, _, _ = fastspeech2.inference(
+  input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+  speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
+  speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+  f0_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
+  energy_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
 )
 
 # melgan inference
-audio_before = melgan.inference(masked_mel_before)[0, :, 0]
-audio_after = melgan.inference(masked_mel_after)[0, :, 0]
+audio_before = melgan.inference(mel_before)[0, :, 0]
+audio_after = melgan.inference(mel_after)[0, :, 0]
 
 # save to file
 sf.write('./audio_before.wav', audio_before, 22050, "PCM_16")
