5748cdcac0e9a5aa6c8021c59df009b7c50b8547
Nixon <voxell.technologies@gmail.com>
2021-01-19 18:54:06 +0800

    converting model weights into tflite models

---
+++ PythonAPI/unity_dependencies/tts_engine/examples/fastspeech2_example.py
@@ -0,0 +1,60 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+# The Original Code is Copyright (C) 2020 Voxell Technologies.
+# All rights reserved.
+
+import soundfile as sf
+import tensorflow as tf
+from tensorflow_tts.inference import AutoConfig
+from tensorflow_tts.inference import TFAutoModel
+from tensorflow_tts.inference import AutoProcessor
+
+processor = AutoProcessor.from_pretrained("../ljspeech_mapper.json")
+
+# initialize fastspeech2 model.
+config = AutoConfig.from_pretrained('../fastspeech2/conf/fastspeech2.v1.yaml')
+fastspeech2 = TFAutoModel.from_pretrained(
+    config=config, 
+    pretrained_path="../fastspeech2/checkpoints/model-150000.h5",
+    is_build=True,
+    name="fastspeech2"
+)
+
+# initialize melgan model
+melgan_config = AutoConfig.from_pretrained('../melgan/conf/melgan.v1.yaml')
+melgan = TFAutoModel.from_pretrained(
+  config=melgan_config,
+  pretrained_path="../melgan/checkpoints/generator-1670000.h5"
+)
+
+input_text = "how much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+input_ids = processor.text_to_sequence(input_text)
+
+# fastspeech2 inference
+mel_before, mel_after, duration_outputs, _, _ = fastspeech2.inference(
+  input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+  speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
+  speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+  f0_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
+  energy_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
+)
+
+# melgan inference
+audio_before = melgan.inference(mel_before)[0, :, 0]
+audio_after = melgan.inference(mel_after)[0, :, 0]
+
+# save to file
+sf.write('./audio_before.wav', audio_before, 22050, "PCM_16")
+sf.write('./audio_after.wav', audio_after, 22050, "PCM_16")
---
+++ PythonAPI/unity_dependencies/tts_engine/tflite_inference.py
@@ -0,0 +1,55 @@
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_tts.inference import AutoProcessor
+
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path='fastspeech_quant.tflite')
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Prepare input data.
+def prepare_input(input_ids):
+  input_ids = tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0)
+  return (input_ids,
+          tf.convert_to_tensor([0], tf.int32),
+          tf.convert_to_tensor([1.0], dtype=tf.float32),
+          tf.convert_to_tensor([1.0], dtype=tf.float32),
+          tf.convert_to_tensor([1.0], dtype=tf.float32))
+
+# Test the model on random input data.
+def infer(input_text):
+  processor = AutoProcessor.from_pretrained(pretrained_path="ljspeech_mapper.json")
+  input_ids = processor.text_to_sequence(input_text.lower())
+  interpreter.resize_tensor_input(input_details[0]['index'], 
+                                  [1, len(input_ids)])
+  interpreter.resize_tensor_input(input_details[1]['index'], 
+                                  [1])
+  interpreter.resize_tensor_input(input_details[2]['index'], 
+                                  [1])
+  interpreter.resize_tensor_input(input_details[3]['index'], 
+                                  [1])
+  interpreter.resize_tensor_input(input_details[4]['index'], 
+                                  [1])
+  interpreter.allocate_tensors()
+  input_data = prepare_input(input_ids)
+  for i, detail in enumerate(input_details):
+    input_shape = detail['shape_signature']
+    interpreter.set_tensor(detail['index'], input_data[i])
+
+  interpreter.invoke()
+
+  # The function `get_tensor()` returns a copy of the tensor data.
+  # Use `tensor()` in order to get a pointer to the tensor.
+  return (interpreter.get_tensor(output_details[0]['index']),
+          interpreter.get_tensor(output_details[1]['index']))
+
+input_text = "Recent research at Harvard has shown meditating\
+for as little as 8 weeks, can actually increase the grey matter in the \
+parts of the brain responsible for emotional regulation, and learning."
+
+decoder_output_tflite, mel_output_tflite = infer(input_text)
+audio_before_tflite = melgan(decoder_output_tflite)[0, :, 0]
+audio_after_tflite = melgan(mel_output_tflite)[0, :, 0]
